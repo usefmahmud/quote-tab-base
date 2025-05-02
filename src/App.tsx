606470e3fc7baa34fc47ad389dcaf547792aabc3
Yousef Mahmoud <usef.mahmoud@outlook.com>
2025-05-02 22:49:50 +0300

    feat(fonts): added gali and greta fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className='bg-background flex h-screen flex-col items-center justify-center text-white'>
-      <h1 className='mb-4 text-4xl font-bold'>Quote Tab Backbone</h1>
+    <div className='bg-background font-greta flex h-screen flex-col items-center justify-center text-white'>
+      <h1 className='mb-4 text-4xl font-light'>تبويب الاقتباسات</h1>
       <h2 className='text-xl'>
         You can find API{' '}
         <a href='/api' className='font-bold text-blue-500 underline'>
