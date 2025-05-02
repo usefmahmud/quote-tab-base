cf36355a0fc312effdd31576f6f3e8191aee3698
Yousef Mahmoud <usef.mahmoud@outlook.com>
2025-05-02 22:14:08 +0300

    initialized the project with required config and packages

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='bg-background flex h-screen flex-col items-center justify-center text-white'>
+      <h1 className='text-4xl font-bold'>Quote Tab Backbone</h1>
+    </div>
+  );
+}
+
+export default App;
